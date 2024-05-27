ed361305fe519a71dcba6ddffae7a0f2cd706e9e
Joshua Morrison <MrM21632@users.noreply.github.com>
2024-05-26 22:28:03 -0400

    SHA-224 and SHA-384 executables

---
+++ sha384sum.cpp
@@ -0,0 +1,27 @@
+#include "sha512.hpp"
+#include <iostream>
+
+
+int main() {
+    std::vector<std::string> test_messages = {
+        "",
+        "a",
+        "abc",
+        "message digest",
+        "the quick brown fox jumps over the lazy dog",
+        "the quick brown fox jumps over the lazy dog.",
+        "abcdefghijklmnopqrstuvwxyz",
+        "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu",
+    };
+
+    SHA384Impl hash;
+    for (std::string message : test_messages) {
+        std::array<uint64_t, 8> result = hash.digest_message(message);
+
+        std::cout << message << ": ";
+        for (int i = 0; i < 6; ++i) {
+            std::cout << std::hex << result[i];
+        }
+        std::cout << std::endl;
+    }
+}
